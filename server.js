a30c8691fc94b0ce51ec6f8c6836f445da173e23
Mukesh <mukesh001kumar0ece@gmail.com>
2024-05-23 16:58:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    // Handle incoming messages
+    console.log('Received message from a client:', message);
+    
+    // Broadcast the message to all connected clients
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
